b4657710c54ee00d725483359036932e1afecefd
zawmoelwin <zawmoelwin@gmail.com>
2016-12-20 01:27:07 -0800

    GPS 2.2 edited

---
+++ ruby/GPS 2.2/gps2_2.rb
@@ -0,0 +1,39 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: seperate the items in an array
+  #
+  # set default quantity will be 0
+  # print the list to the console [can you use one of your other methods here?]
+# output: hash with the key as the item and value as the quantity
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: list will be the name of the hash
+ 	# item will be the key
+ 	#quantity will be the value
+# output:the hash with added hash
+
+ # list = {
+ # item: quantity
+ # }
+
+
+# Method to remove an item from the list
+# input:list and the item
+# steps: iterate through the hash for the key
+ 	# remove the item key
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: list, item, new quantity
+# steps:iterate through the list to find the item
+ 	#update the quantity of item
+# output:updated hash
+
+# Method to print a list and make it look pretty
+# input:list
+# steps:iterate through print
+ # Here is your list:
+ # list name
+ # item : quantity
+# output: pretty printed list
--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,14 +1,33 @@
-my_box = {
+#nested data  for a box
+#first level hash (need to access with key)
+# second level array (need to access with array index)
+#third level hash
+#4th level hash
+#5th level array
 
-  {diary:}
-  {old_photos:{family:{
-    parents: "father"
+my_box = { box:
+#  {diary:}
+  [old_photos:{family:{
+    parents: ["father","mother"]
       }
-    }
-  }
-  {old_wallet:{ pocketphoto: "my girlfriend"
+    }]
 
-    }
-  }
 
 }
+
+
+#printing the mother photos
+p my_box [:box][0][:old_photos][:family][:parents][1]
+
+
+# add photo in box old_photos family
+
+p my_box [:box][0][:old_photos][:family].merge! ({siblings: ["sister"]})
+
+z = {key: "data"}
+y= {answer: "data"}
+z.merge! (y)
+p z
+
+# add brothers photo in sibling category
+p my_box [:box][0][:old_photos][:family][:siblings].push "brother"
