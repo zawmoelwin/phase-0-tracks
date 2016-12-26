74ea4b26f776f88b6ba8a4b8ba4b55f2450b2ca7
zawmoelwin <zawmoelwin@gmail.com>
2016-12-25 19:59:22 -0800

    partial submission

---
+++ ruby/santa.rb
@@ -0,0 +1,36 @@
+
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies (cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+  def initialize (gender , ethanicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethanicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
+
+end
+
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+#
+# gender, which will be a string passed in on initialization
+# ethnicity, which will be a string passed in on initialization
+# reindeer_ranking, an array of reindeer from most preferred to least preferred. This is not passed in on initialization; it simply gets a default value of ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+# age, which is not passed in on initialization and defaults to 0
