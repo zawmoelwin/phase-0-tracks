56fa8f4700bebc5bd2d832c301de58ca5791c5b5
Zaw Moe Lwin <zawmoelwin@gmail.com>
2016-12-25 23:35:41 -0800

    add flight.rb and shout.rb

---
+++ ruby/shout.rb
@@ -0,0 +1,35 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#   def self.yell_happily(worlds)
+#   	words + "!!!" + " :)"
+#   end
+# end
+
+# Comment out the old code in shout.rb, and make a mixin version of the Shout module instead.
+# Write two classes representing anything that might shout, and include the Shout module in those classes.
+# Test your work by adding driver code at the bottom of the file that instantiates instances of your classes and calls the two module methods on each instance.
+
+module Shout
+  def yell_painfully(words)
+    "Ouch! " + words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+  	"Yay! "+ words + "!!!" + " :)"
+  end
+end
+
+class Check_result
+	include Shout
+end
+
+class Fall
+	include Shout
+end
+
+x = Check_result.new
+puts x.yell_happily("I passed")
+y = Fall.new
+puts y.yell_painfully("Ah yo")
