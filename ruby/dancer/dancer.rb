d412991e33f5c2f930a40afc5e6cb044b1c3d8b7
Zaw Moe Lwin <zawmoelwin@gmail.com>
2017-01-03 02:26:03 -0800

    add all features

--- ruby/dancer/dancer.rb
+++ ruby/dancer/dancer.rb
@@ -1,9 +1,6 @@
-
-
-
 class Dancer
 
-  attr_reader   :name 
+  attr_reader   :name, :card
   attr_accessor :age
 
   def initialize(name, age)
@@ -11,7 +8,30 @@ class Dancer
     @age = age
   end
 
-  
+  def pirouette
+    p "*twirls*"
+  end
+
+  def bow
+    p "*bows*"
+  end
+
+  def queue_dance_with(name)
+    @card||=[]
+    @card << name
+  end
+
+
+  def begin_next_dance
+    temp = "Now dancing with #{card[0]}."
+    card.shift
+    p temp
+  end
+
+  def jump(int)
+    temp = int
+    p "Jumps and turn #{temp} times."
+  end
 
 
 end
--- ruby/dancer/dancer_spec.rb
+++ ruby/dancer/dancer_spec.rb
@@ -14,4 +14,31 @@ describe Dancer do
     dancer.age = 34
     expect(dancer.age).to eq 34
   end
+  it "twirls" do
+    expect(dancer.pirouette).to eq "*twirls*"
+  end
+
+  it "bows to a partner" do
+    expect(dancer.bow).to eq "*bows*"
+  end
+
+  it "can add parters to the dance card queue" do
+  dancer.queue_dance_with("Mikhail Baryshnikov")
+  expect(dancer.card).to eq ["Mikhail Baryshnikov"]
+  dancer.queue_dance_with("Anna Pavlova")
+  expect(dancer.card).to eq ["Mikhail Baryshnikov", "Anna Pavlova"]
+  end
+
+  it "can start the next dance in the queue" do
+    dancer.queue_dance_with("Mikhail Baryshnikov")
+    dancer.queue_dance_with("Anna Pavlova")
+    expect(dancer.begin_next_dance).to eq "Now dancing with Mikhail Baryshnikov."
+    expect(dancer.card).to eq ["Anna Pavlova"]
+  end
+
+  it "dancing and jumping" do
+    expect(dancer.jump(4)).to eq "Jumps and turn 4 times."
+  end
+
+
 end
