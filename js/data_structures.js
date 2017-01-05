e4db3b7da7a4c2490e98ee5daf66e60e31ae3fde
Zaw Moe Lwin <zawmoelwin@gmail.com>
2017-01-04 18:08:57 -0800

    release 1 for loop

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,5 +1,14 @@
-var arrary1 = [ "blue" ,"yellow" ,"red" , "green"] ;
+var array1 = [ "blue" ,"yellow" ,"red" , "green"] ;
 var array2 = ["Steven" ,"Julie" ,"Joe" ,"Alex"] ;
 
 array1.push("purple");
 array2.push("Fred");
+
+var horse = {};
+
+for (var i=0; i<array1.length; i++)
+{
+	horse[array2[i]] = array1[i];
+}
+
+console.log(horse);
